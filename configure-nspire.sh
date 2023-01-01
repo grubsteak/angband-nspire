fe083411193a4071d84ff90c271c552bbaee8087
grubsteak <11185290+grubsteak@users.noreply.github.com>
2023-01-01 21:22:03 +0000

    Add comment

--- configure-nspire.sh
+++ configure-nspire.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 autoreconf -fi;
+
+# horrible C flags lol
 CFLAGS='-DPRIVATE_USER_PATH=\"/documents/lib/dumps/\" -DDEFAULT_CONFIG_PATH=\"/documents/lib\" -DDEFAULT_LIB_PATH=\"/documents/lib\" -DDEFAULT_DATA_PATH=\"/documents/lib\"' \
     CC=nspire-gcc \
     ./configure --host=arm-none-eabi --with-no-install;
